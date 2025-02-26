ba5cfbc8a648506d1f6e62193d6697fa1008f54f
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-02-26 12:41:58 -0300

    feat: adição de estilos e elementos visuais

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,14 +5,14 @@ export function Navbar() {
     <header className="flex h-16 w-full items-center border-b">
       <div className="mx-auto flex w-full max-w-screen-2xl items-baseline px-4 2xl:px-0">
         <NavLink to="/" className="text-2xl font-bold">
-          movies<span className="text-violet-700">Nook</span>
+          movies<span className="text-primary">Nook</span>
         </NavLink>
 
         <NavLink
           to="/watchlist"
           className={({ isActive }) =>
             isActive
-              ? "ml-auto font-semibold text-violet-500"
+              ? "ml-auto font-semibold text-primary/80"
               : "ml-auto font-semibold"
           }
         >
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,15 @@
+import { Input } from "@/components/ui/input"
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <section className="mx-auto flex flex-col items-center gap-10">
+        <h2 className="animate-fade-in max-w-screen-md bg-gradient-to-r from-blue-400 to-violet-900 bg-clip-text pb-4 text-center text-4xl font-semibold text-transparent opacity-0 sm:text-5xl">
+          moviesNook. Discover and save your favorite movies!
+        </h2>
+
+        <Input placeholder="Search for movies" className="max-w-screen-sm" />
+      </section>
+    </div>
+  )
 }
